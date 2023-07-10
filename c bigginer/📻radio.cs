7deafa696d7afc35697c77c6e0257a0d8183352a
Andrethy  David Aguilar Ariza <72534486+andrethydavid@users.noreply.github.com>
2023-07-10 10:15:05 -0500

    Add files via upload

---
+++ c bigginer/📻radio.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//pi *radio*radio
+
+var radio = 0d;
+var resuldato = 1.1;
+
+const double pi = 3.14;
+
+const double SiempreseescribeAsi = 200.0d;
+
+Console.WriteLine("BIenvenido, calculemos circulos!");
+Console.WriteLine("Ingrese el valor del radio");
+radio = Convert.ToDouble(Console.ReadLine());
+
+
+resuldato = Math.PI * radio * radio;
+
+Console.WriteLine("el area de tu circulo es de:" +resuldato);
---
+++ c bigginer/🔢programa dearea de triangulo VAR.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, david!");
+
+// porgrama que calcula el area de un rectangulo
+var ladoA = 1.1;  //declaracionde valores, variable de forma implicita 
+var ladoB = 1.1 ; //bulioanos
+var resultado = 1.1;
+
+Console.WriteLine("calcula el area de un rectangulo");
+Console.WriteLine("Ingrese el valor del lado A");
+ladoA = Convert.ToDouble(Console.ReadLine()); // touble para valores bulianos 
+
+Console.WriteLine("ingrese el valor del lado B");
+ladoB = Convert.ToDouble(Console.ReadLine());
+
+ladoA++; //incremento 
+
+resultado = ladoA * ladoB;
+Console.WriteLine("El lado A es:" +ladoA +"El lado B es:" +ladoB +"El resultado es" +resultado);
